670219932c6c2a317931a2d952cd6c6fa23a4a26
William Fisher <ashton0312@gmail.com>
2020-04-29 22:42:41 -0400

    Added message that shows amount due if paying through venmo or cash app

--- index.js
+++ index.js
@@ -128,7 +128,8 @@ const handleReaction = (reaction, user) => {
                 Promise.all([
                   channel.send(`You selected [${response.items[item].name}]`),
                   channel.send(`We have applied your discount: ${discount}%!`),
-                  channel.send(`To purchase with paypal click this link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=ashton0312%40gmail%2ecom&lc=US&item_name=Sloppy%20Ark&amount=${(100-discount)/100 * (response.items[item].price)}%2e00&currency_code=USD&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHosted`)
+                  channel.send(`To purchase with paypal click this link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=ashton0312%40gmail%2ecom&lc=US&item_name=Sloppy%20Ark&amount=${(100-discount)/100 * (response.items[item].price)}%2e00&currency_code=USD&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHosted`),
+                  channel.send(`You can also purchase by sending $${(100-discount)/100 * (response.items[item].price)} via Venmo or Cash App`)
                 ]).catch(console.error);
               } else {
                 Promise.all([
