cc63772226884f93c8f8f9f41063bc0b99980e57
William Fisher <ashton0312@gmail.com>
2020-05-03 19:08:34 -0400

    Moved methods to seperate file and updated to use async/await

--- index.js
+++ index.js
@@ -1,153 +1,40 @@
-require("dotenv").config()
-const ps = require('pg')
-const shopData = require("./categories.json")
-const lootboxes = require("./lootboxes.json")
-const Discord = require("discord.js")
-const client = new Discord.Client()
-
-const db = new ps.Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
-db.connect();
-
-let donationCount = 1;
-let lootBoxIndex = 0;
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const methods = require("./methods");
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-client.on("message", msg => {
-  if(msg.content === "!setupShop") {
-    const filter = (reaction, user) => {
-      return reaction.emoji.name === '💚';
-    };
-    msg.channel.send('Click The Heart To Donate!')
-    .then(message => {
-      message.react('💚');
-    })
-  }
-  if(msg.content === "!loot" && msg.member.hasPermission('ADMINISTRATOR')) {
-    msg.delete();
-    msg.channel.send(`**[Loot Box]** \n${lootboxes[lootBoxIndex].latitude} Latitude\n${lootboxes[lootBoxIndex].longitude} Longitude\n*Hurry and find it before someone else does!*`)
-    .then(() => {
-      lootBoxIndex++;
-    })
-    .catch(console.error);
-  }
-  if(msg.content.startsWith("!setIndex") && msg.member.hasPermission('ADMINISTRATOR')) {
-    msg.delete();
-    let index = msg.content.split(' ')[1];
-    lootBoxIndex = index;
-    msg.reply(`Set index of lootboxs to ${lootBoxIndex}!`)
-    .catch(console.error);
-  }
-  if(msg.content === "!spin") {
-    db.query(`SELECT author FROM discounts WHERE author='${msg.author.id}'`, (err, res) => {
-      if (err) console.log(err);
-      if(res.rows.length > 0) {
-        msg.reply('Hey! You already spun the wheel!');
-      } else {
-        randomInt = getRandomInt();
-      msg.reply(`Congrats! You won ${randomInt}% off in the store! Your winnings have been saved and you can start an order by going to #shop and clicking the reaction!`)
-      .catch(console.error);
-      db.query(`INSERT INTO discounts VALUES (${msg.author.id}, ${randomInt})`)
-      .then(console.log(`Wrote id: ${msg.author.id}, discount: ${randomInt} to file!`))
-      .catch(console.error);
-      }
-    })
-  }
-})
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.on('messageReactionAdd', (reaction, user) => {
-  if(reaction.emoji.name === '💚' && reaction.message.author.id == client.user.id) {
-    handleReaction(reaction, user);
+client.on("message", (msg) => {
+  if (
+    msg.content === "!setupShop" &&
+    msg.member.hasPermission("ADMINISTRATOR")
+  ) {
+    methods.handleSetupShop(msg);
   }
-})
-client.login(process.env.BOT_TOKEN)
+  if (msg.content === "!loot" && msg.member.hasPermission("ADMINISTRATOR")) {
+    methods.handleLoot(msg);
+  }
+  if (
+    msg.content.startsWith("!setIndex") &&
+    msg.member.hasPermission("ADMINISTRATOR")
+  ) {
+    methods.handleSetIndex(msg);
+  }
+  if (msg.content === "!spin") {
+    methods.handleSpin(msg);
+  }
+});
 
+client.on("messageReactionAdd", (reaction, user) => {
+  if (
+    reaction.emoji.name === "💚" &&
+    reaction.message.author.id === client.user.id
+  ) {
+    methods.handleReaction(reaction, user);
+  }
+});
 
-const handleReaction = (reaction, user) => {
-  if(reaction.message.author.id === user.id) return;
-  var reactUser = user;
-  reaction.users.remove(user.id);
-  reaction.message.guild.channels.create(`donation-${donationCount}`, {
-    type: 'text',
-    permissionOverwrites: [
-      {
-        id: user.id,
-        allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-      },
-      {
-        id: reaction.message.guild.roles.everyone,
-        deny: ['VIEW_CHANNEL'],
-      },
-      {
-        id: client.user.id,
-        allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-      },
-    ],
-  })
-  .then((channel) => {
-    donationCount++;
-    channel.send('Hi! Start by typing the category number you would like to purchase from!', { reply: reactUser })
-    .then(() => {
-      let categoryString = "";
-      for(i=0; i < shopData.length; i++) {
-        categoryString = categoryString + `[${shopData[i].id}] ${shopData[i].name}\n`;
-      }
-      channel.send(categoryString)
-      .catch(console.error);
-      const filter = m => !isNaN(m.content);
-      channel.awaitMessages(filter, { max: 1 })
-      .then((collected) => {
-        collected.first().delete().catch(console.error);
-        let input = collected.first().content;
-        let channel = collected.first().channel;
-        let response = shopData.find(element => element.id == input)
-        channel.send(`You selected [${response.name}]. Which item would you like?`)
-        let itemString = "";
-        for(i = 0; i < response.items.length; i++) {
-          itemString = itemString + `[${input}${i}] ${response.items[i].name} | Price: $${response.items[i].price}\n`
-        }
-        channel.send(itemString)
-        .then((message) => {
-          const filter = m => !isNaN(m.content);
-          message.channel.awaitMessages(filter, { max: 1 })
-          .then((collected) => {
-            collected.first().delete().catch(console.error);
-            let category = collected.first().content.substring(0,1);
-            let item = collected.first().content.substring(1);
-            let channel = collected.first().channel;
-            let response = shopData.find(element => element.id == category)
-            db.query(`SELECT discount FROM discounts WHERE author='${collected.first().author.id}'`)
-            .then(res => {
-              if(res.rows.length > 0) {
-                let discount = res.rows[0].discount;
-                Promise.all([
-                  channel.send(`You selected [${response.items[item].name}]`),
-                  channel.send(`We have applied your discount: ${discount}%!`),
-                  channel.send(`To purchase with paypal click this link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=ashton0312%40gmail%2ecom&lc=US&item_name=Sloppy%20Ark&amount=${(100-discount)/100 * (response.items[item].price)}%2e00&currency_code=USD&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHosted`),
-                  channel.send(`You can also purchase by sending $${(100-discount)/100 * (response.items[item].price)} via Venmo or Cash App`)
-                ]).catch(console.error);
-              } else {
-                Promise.all([
-                  channel.send(`You selected [${response.items[item].name}]`),
-                  channel.send(`To purchase with paypal click this link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=ashton0312%40gmail%2ecom&lc=US&item_name=Sloppy%20Ark&amount=${response.items[item].price}%2e00&currency_code=USD&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHosted`)
-                ]).catch(console.error);
-              }
-            }).catch(console.error);
-          }).catch(console.log);
-        }).catch(console.log);
-      }).catch(console.log);
-    }).catch(console.log);
-  }).catch(console.error);
-}
-
-const getRandomInt = () => {
-  min = Math.ceil(10);
-  max = Math.floor(50);
-
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+client.login(process.env.BOT_TOKEN);
---
+++ methods.js
@@ -0,0 +1,181 @@
+require("dotenv").config();
+const ps = require("pg");
+const shopData = require("./categories.json");
+const lootboxes = require("./lootboxes.json");
+
+const db = new ps.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+db.connect();
+
+// TODO: Update to store counts in the database.
+let donationCount = 1;
+let lootBoxIndex = 0;
+
+const channelPermissions = {
+  type: "text",
+  permissionOverwrites: [
+    {
+      id: user.id,
+      allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"],
+    },
+    {
+      id: reaction.message.guild.roles.everyone,
+      deny: ["VIEW_CHANNEL"],
+    },
+    {
+      id: client.user.id,
+      allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"],
+    },
+  ],
+};
+
+const getRandomInt = () => {
+  min = Math.ceil(10);
+  max = Math.floor(50);
+
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const handleReaction = async (reaction, user) => {
+  if (reaction.message.author.id === user.id) return;
+  reaction.users.remove(user.id);
+  try {
+    const channel = await reaction.message.guild.channels.create(
+      `donation-${donationCount}`,
+      channelPermissions
+    );
+    donationCount++;
+    await channel.send(
+      "Hi! Start by typing the category number you would like to purchase from!",
+      { reply: user }
+    );
+
+    let categoryString = "";
+    for (i = 0; i < shopData.length; i++) {
+      categoryString =
+        categoryString + `[${shopData[i].id}] ${shopData[i].name}\n`;
+    }
+
+    await channel.send(categoryString);
+    const isNumFilter = (m) => !isNaN(m.content);
+    let collectedMessage = await channel.awaitMessages(isNumFilter, { max: 1 });
+    await collectedMessage.first().delete({ timeout: 10000 });
+
+    let collectedMessageContent = collectedMessage.first().content;
+    const category = shopData.find(
+      (element) => element.id == collectedMessageContent
+    );
+    await channel.send(
+      `You selected [${response.name}]. Which item would you like?`
+    );
+
+    let itemString = "";
+    for (i = 0; i < category.items.length; i++) {
+      itemString =
+        itemString +
+        `[${i}] ${category.items[i].name} | Price: $${category.items[i].price}\n`;
+    }
+
+    await channel.send(itemString);
+    collectedMessage = await channel.awaitMessages(filter, { max: 1 });
+    await collectedMessage.first().delete({ timeout: 10000 });
+    collectedMessageContent = collected.first().content;
+
+    const response = await db.query(
+      `SELECT discount FROM discounts WHERE author='${
+        collectedMessageContent.first().author.id
+      }'`
+    );
+    if (response.rows.length > 0) {
+      const discount = response.rows[0].discount;
+      const total =
+        ((100 - discount) / 100) *
+        category.items[collectedMessageContent].price;
+      await channel.send(
+        `You selected [${category.items[collectedMessageContent].name}]`
+      );
+      await channel.send(`We have applied your discount: ${discount}%!`);
+      await channel.send(
+        `To purchase with paypal click this link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=ashton0312%40gmail%2ecom&lc=US&item_name=Sloppy%20Ark&amount=${total}%2e00&currency_code=USD&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHosted`
+      );
+      await channel.send(
+        `You can also purchase by sending $${total} via Venmo or Cash App`
+      );
+    } else {
+      await channel.send(
+        `You selected [${category.items[collectedMessageContent].name}]`
+      );
+      await channel.send(
+        `To purchase with paypal click this link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=ashton0312%40gmail%2ecom&lc=US&item_name=Sloppy%20Ark&amount=${category.items[collectedMessageContent].price}%2e00&currency_code=USD&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHosted`
+      );
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const handleSpin = async (msg) => {
+  try {
+    const response = await db.query(
+      `SELECT author FROM discounts WHERE author='${msg.author.id}'`
+    );
+    if (response.rows.length > 0) {
+      await msg.reply("Hey! You already spun the wheel!");
+    } else {
+      const randomInt = getRandomInt();
+      await msg.reply(
+        `Congrats! You won ${randomInt}% off in the store! Your winnings have been saved and you can start an order by going to #shop and clicking the reaction!`
+      );
+      await db.query(
+        `INSERT INTO discounts VALUES (${msg.author.id}, ${randomInt})`
+      );
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const handleSetIndex = async (msg) => {
+  try {
+    await msg.delete();
+    const index = msg.content.split(" ")[1];
+    if (index < lootboxes.length) {
+      lootBoxIndex = index;
+      await msg.reply(`Set index of lootboxs to ${lootBoxIndex}!`);
+    } else {
+      await msg.reply(`Error. Max index is ${lootboxes.length - 1}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const handleLoot = async (msg) => {
+  try {
+    await msg.delete();
+    await msg.channel.send(
+      `**[Loot Box]** \n${lootboxes[lootBoxIndex].latitude} Latitude\n${lootboxes[lootBoxIndex].longitude} Longitude\n*Hurry and find it before someone else does!*`
+    );
+    lootBoxIndex++;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const handleSetupShop = async (msg) => {
+  try {
+    const message = await msg.channel.send("Click The Heart To Donate!");
+    await message.react("💚");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.handleReaction = handleReaction;
+exports.handleSpin = handleSpin;
+exports.handleSetIndex = handleSetIndex;
+exports.handleLoot = handleLoot;
+exports.handleSetupShop = handleSetupShop;
