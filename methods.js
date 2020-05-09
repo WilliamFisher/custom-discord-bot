3a9648c79f6bae69ca55fc8de999c85f2703cc62
William Fisher <ashton0312@gmail.com>
2020-05-09 19:39:07 -0400

    Traced undefined suggestionUser to a missing await

--- methods.js
+++ methods.js
@@ -293,8 +293,7 @@ const handleUpdateSuggestion = async (msg, client, status) => {
   const userID = suggestionMessage.embeds[0].footer.text.split(/ +/)[2];
   const suggestionUser = client.users.fetch(userID);
 
-  console.log(suggestionMessage.embeds[0].footer.text.split(/ +/));
-  console.log(`${userID} | ${suggestionUser.id}`);
+  console.log(suggestionUser);
 
   let embedColor = "";
   if (status === 0) {
